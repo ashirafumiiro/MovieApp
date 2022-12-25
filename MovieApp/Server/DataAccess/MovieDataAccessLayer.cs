9215c76fcec176d3950b8f4781106c7e2c830af6
Miiro Ashirafu <ashirafumiiro@gmail.com>
2022-12-25 09:29:07 +0300

    Add project files.

---
+++ MovieApp/Server/DataAccess/MovieDataAccessLayer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApp.Server.Interfaces;
+using MovieApp.Server.Models;
+using MovieApp.Shared.Models;
+
+namespace MovieApp.Server.DataAccess
+{
+    public class MovieDataAccessLayer : IMovie
+    {
+        readonly MovieDBContext _dbContext;
+
+        public MovieDataAccessLayer(IDbContextFactory<MovieDBContext> dbContext)
+        {
+            _dbContext = dbContext.CreateDbContext();
+        }
+
+        public async Task<List<Genre>> GetGenre()
+        {
+            return await _dbContext.Genres.AsNoTracking().ToListAsync();
+        }
+    }
+}
